16d2a78725f60a8ab7bc2e254f95dc3ab5cde8d5
Julissa <julissaj@outlook.com>
2016-03-14 12:03:29 -0700

    Updated views/stories directory. Added Stories controller. Updated routes to include characters resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # root 'stories#index'
 
-  resources :movies
+  resources :stories
+  resources :characters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
