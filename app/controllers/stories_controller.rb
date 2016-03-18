21ebdf9a953f0d4981499077225bdde9bf634507
Julissa <julissaj@outlook.com>
2016-03-18 12:52:09 -0700

    Ordered results alphabetically

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,7 +4,7 @@ class StoriesController < ApplicationController
     if params[:search]
       @stories = Story.search(params[:search])
     elsif params[:media_format]
-      @stories = Story.where(:media_format => params[:media_format])
+      @stories = Story.where(:media_format => params[:media_format]).order("title ASC")
     # else
     #   flash[:notice] = "There are no <b>stories</b> in this category".html_safe
     end
